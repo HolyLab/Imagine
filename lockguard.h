81c075b6ad92670183a62ed6e28d9897b24e6434
zsguo <zsguo@44b8eedb-1f3a-4159-9d21-a6cf1bf24d9b>
2010-10-20 16:04:22 +0000

    added the lockguard file under svn ctrl

---
+++ lockguard.h
@@ -0,0 +1,28 @@
+#ifndef lockguard_h
+#define lockguard_h
+
+#include <QMutex>
+
+class CLockGuard {
+public:
+   CLockGuard(QMutex* apLock):mpLock(apLock),mOwned(false){
+      mpLock->lock();
+      mOwned=true;
+   }
+   ~CLockGuard(){
+      if(mOwned)mpLock->unlock();
+   }
+
+private:
+   QMutex * mpLock;
+   bool mOwned;
+
+   //forbid copy assignment and initialization
+   CLockGuard& operator= (const CLockGuard &);
+   CLockGuard(const CLockGuard &);
+
+};//class
+
+
+#endif //#ifndef
+ 
