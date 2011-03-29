135da8098a62c853537f3c4d8dde810ab857de19
zsguo <zsguo@44b8eedb-1f3a-4159-9d21-a6cf1bf24d9b>
2011-03-29 21:14:13 +0000

    more changes

--- cooke.hpp
+++ cooke.hpp
@@ -3,6 +3,26 @@
 
 #include "camera_g.hpp"
 
-#include "
+#include "SC2_CamExport.h"
+#include "sc2_SDKStructures.h"
+#include "sc2_defs.h"
+#include "PCO_err.h"
+#define PCO_ERRT_H_CREATE_OBJECT
+#include "PCO_errt.h"
+
+
+class CookeCamera: public Camera {
+
+public:
+   CookeCamera(){
+      vendor="cooke";
+   }
+
+   ~CookeCamera(){
+
+   }
+
+
+};//class, CookeCamera
 
 #endif //COOKE_HPP
