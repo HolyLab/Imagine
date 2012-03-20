8c52d080a1a7489673df0a6944b417137f2c39af
zsguo <kuai98@gmail.com>
2012-03-20 15:53:37 -0500

    mod the cfg to use "daq" setting

--- imagine.js
+++ imagine.js
@@ -5,6 +5,10 @@ var camera = "cooke";
 
 //piezo/stage:
 var positioner = "dummy";
-//var positioner = "volpiezo";
+//var positioner = "volpiezo"; //NOTE: requires daq=="ni"
 //var positioner = "pi";
 //var positioner = "thor";
+
+//daq:
+var daq="dummy";
+//var daq="ni";
