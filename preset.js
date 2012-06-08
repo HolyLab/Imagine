642d3ae31d8dfefbc9a24cc184a114fcc55d286f
jason <jason@joop.wustl.edu>
2012-06-08 15:37:41 -0500

    begin to introduce mech in which user can specify preset values

---
+++ preset.js
@@ -0,0 +1,15 @@
+//hold preset params when imagine initially load
+
+var params={
+   ///positioner's
+   startPosition:100,
+   stopPosition:400,
+   travelBackTime:0,
+
+   ///camera's
+   numOfStacks:5,
+   framesPerStack:89,
+   exposureTime:0.0107,
+   idleTime:0.7
+};
+
