626134679ca1b98d167ebd9f1f6978d2754d5717
zsguo <kuai98@gmail.com>
2012-08-27 16:20:18 -0500

    now in the cfg files, use diff settings for different rigs

--- preset.js
+++ preset.js
@@ -1,16 +1,51 @@
 //hold preset params when imagine initially load
 
-var preset={
-   ///positioner's
-   startPosition:100,
-   stopPosition:400,
-   travelBackTime:0,
+if(rig=="hs-ocpi"){
+   var preset={
+      ///positioner's
+      startPosition:100,
+      stopPosition:400,
+      travelBackTime:0,
 
-   ///camera's
-   numOfStacks:5,
-   framesPerStack:89,
-   exposureTime:0.0107,
-   idleTime:0.7,
-   triggerMode: "internal", //internal or external
-};
+      ///camera's
+      numOfStacks:5,
+      framesPerStack:89,
+      exposureTime:0.0107,
+      idleTime:0.7,
+      triggerMode: "internal", //internal or external
+   };
+}
+
+if(rig=="bakewell"){
+   var preset={
+      ///positioner's
+      startPosition:100,
+      stopPosition:400,
+      travelBackTime:0,
+
+      ///camera's
+      numOfStacks:5,
+      framesPerStack:200,
+      exposureTime:0.02,
+      idleTime:0.7,
+      triggerMode: "internal", //internal or external
+   };
+}
+
+if(rig=="ocpi-1"){
+   var preset={
+      ///positioner's
+      startPosition:100,
+      stopPosition:400,
+      travelBackTime:0,
+
+      ///camera's
+      numOfStacks:5,
+      framesPerStack:20,
+      exposureTime:0.03,
+      idleTime:0.7,
+      triggerMode: "external", //internal or external
+   };
+}
+
 
