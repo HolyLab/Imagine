8dc7f6c7f8f8cde984f7558b46296759845b0500
zsguo <kuai98@gmail.com>
2013-01-17 15:18:47 -0600

    for the avt camera, allow controlling shutter in js

--- avt.js
+++ avt.js
@@ -1,1 +1,23 @@
-//just a place holder
+var onShutterOpen = function () {
+   var stackIdx=this.currentStackIndex;
+   var line;
+   if(stackIdx%2==0) line=6;
+   else line=7;
+
+   var result=setDio(line, true);
+
+   return result;
+};
+
+
+var onShutterClose = function () {
+   var stackIdx=this.currentStackIndex;
+   var line;
+   if(stackIdx%2==0) line=6;
+   else line=7;
+
+   var result=setDio(line, false);
+
+   return result;
+};
+
