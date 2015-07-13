9234c7fb14586dd1de64d5242511d48215614e02
Ben Acland <benacland@gmail.com>
2015-07-13 15:40:59 -0400

    camera config for ocpi 2

---
+++ scripts/ocpi-2.js
@@ -0,0 +1,24 @@
+var checkRoi = function () {
+    var hstart = this.hstart;
+    var hend = this.hend;
+    var vstart = this.vstart;
+    var vend = this.vend;
+
+    return hstart < hend && vstart < vend &&
+        (hstart - 1) % 160 == 0 && (2560 - hend) % 160 == 0 &&
+        vstart - 1 == 2160 - vend;
+};
+
+var onShutterOpen = function () {
+    var result = setDio(4, true);
+
+    return result;
+};
+
+
+var onShutterClose = function () {
+    var result = setDio(4, false);
+
+    return result;
+};
+
